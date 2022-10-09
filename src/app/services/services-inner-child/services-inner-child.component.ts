abb2506af73b1accf46a5da7f7bf6102d15976ea
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-10-08 23:27:00 -0400

    add services inner child component

---
+++ src/app/services/services-inner-child/services-inner-child.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services-inner-child',
+  templateUrl: './services-inner-child.component.html',
+  // styleUrls: ['./services-inner-child.component.css'],
+  styles: ['.container {background-color: #fff;}']
+})
+export class ServicesInnerChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
