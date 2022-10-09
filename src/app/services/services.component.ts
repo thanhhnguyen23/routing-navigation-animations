e6f1f1f8ed3baa2645f4268c21d4a34b57d9fea7
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-10-08 23:28:00 -0400

    add services component

---
+++ src/app/services/services.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  templateUrl: './services.component.html',
+  styleUrls: ['./services.component.css'],
+  styles: ['.container {background-color:#fff;}']
+})
+export class ServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
