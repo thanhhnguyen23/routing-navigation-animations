38587d6f597b99048a3f7e11eb88fe157798aa7e
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-10-08 23:20:53 -0400

    add custom modules (products, services, about)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AboutRoutingModule } from './about/about-routing.module';
+import { AboutModule } from './about/about.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsModule } from './products/products.module';
+import { ServicesModule } from './services/services.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ProductsModule,
+    ServicesModule,
+    AboutModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
