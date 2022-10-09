9516facdb71767ba6b612d344d149fb0037fdc34
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-10-09 00:01:25 -0400

    update project name globally across project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/routing-navigation'),
+      dir: require('path').join(__dirname, './coverage/routing-navigation-animations'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'routing-navigation';
+  title = 'examples of routing // navigation // angular animations';
 }
