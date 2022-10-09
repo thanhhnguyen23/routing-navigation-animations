8076c776cc17847afc88b59b93dbedc9eb6ba032
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-10-08 23:27:30 -0400

    add child component

---
+++ src/app/services/services-child/services-child.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services-child',
+  templateUrl: './services-child.component.html',
+  // styleUrls: ['./services-child.component.css'],
+  styles: ['.container {background-color: #fff;}']
+})
+export class ServicesChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
